ea996f3c28a4ad6d7173edac05e2faadb9a0ac3a
Duc Nguyen <minhduc140583@gmail.com>
2024-10-27 00:59:11 +0700

    Fix bug decodeFromForm

--- src/reflect.ts
+++ src/reflect.ts
@@ -1,49 +1,49 @@
 export function valueOf(obj: any, key: string): any {
-  const mapper = key.split('.').map(item => {
-    return item.replace(/\[/g, '.[').replace(/\[|\]/g, '');
-  });
-  const reSplit = mapper.join('.').split('.');
+  const mapper = key.split(".").map((item) => {
+    return item.replace(/\[/g, ".[").replace(/\[|\]/g, "")
+  })
+  const reSplit = mapper.join(".").split(".")
   return reSplit.reduce((acc, current, index, source) => {
-    const value = getDirectValue(acc, current);
+    const value = getDirectValue(acc, current)
     if (!value) {
-      source.splice(1);
+      source.splice(1)
     }
-    return value;
-  }, obj);
+    return value
+  }, obj)
 }
 export function getDirectValue(obj: any, key: string): any {
   if (obj && obj.hasOwnProperty(key)) {
-    return obj[key];
+    return obj[key]
   }
-  return null;
+  return null
 }
 export function setValue(obj: any, key: string, value: any): any {
-  let replaceKey = key.replace(/\[/g, '.[').replace(/\.\./g, '.');
-  if (replaceKey.indexOf('.') === 0) {
-    replaceKey = replaceKey.slice(1, replaceKey.length);
+  let replaceKey = key.replace(/\[/g, ".[").replace(/\.\./g, ".")
+  if (replaceKey.indexOf(".") === 0) {
+    replaceKey = replaceKey.slice(1, replaceKey.length)
   }
-  const keys = replaceKey.split('.');
+  const keys = replaceKey.split(".")
   let firstKey = keys.shift()
   if (!firstKey) {
-    return;
+    return
   }
-  const isArrayKey = /\[([0-9]+)\]/.test(firstKey);
+  const isArrayKey = /\[([0-9]+)\]/.test(firstKey)
   if (keys.length > 0) {
-    const firstKeyValue = obj[firstKey] || {};
-    const returnValue = setValue(firstKeyValue, keys.join('.'), value);
-    return setKey(obj, isArrayKey, firstKey, returnValue);
+    const firstKeyValue = obj[firstKey] || {}
+    const returnValue = setValue(firstKeyValue, keys.join("."), value)
+    return setKey(obj, isArrayKey, firstKey, returnValue)
   }
-  return setKey(obj, isArrayKey, firstKey, value);
+  return setKey(obj, isArrayKey, firstKey, value)
 }
 const setKey = (_object: any, _isArrayKey: boolean, _key: string, _nextValue: any) => {
   if (_isArrayKey) {
     if (_object.length > _key) {
-      _object[_key] = _nextValue;
+      _object[_key] = _nextValue
     } else {
-      _object.push(_nextValue);
+      _object.push(_nextValue)
     }
   } else {
-    _object[_key] = _nextValue;
+    _object[_key] = _nextValue
   }
-  return _object;
-};
+  return _object
+}
